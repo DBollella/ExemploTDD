fd90585542931b8e9b32546d5bd8bdc6a8b7bbfd
DBollella <douglas.bollella@gmail.com>
2018-09-02 23:26:17 -0300

    Initial

---
+++ ExemploTDD.Tests/Negocios/OperacoesMatematicasBO_Test.cs
@@ -0,0 +1,24 @@
+﻿using ExemploTDD.Negocios;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploTDD.Tests.Negocios
+{
+    [TestFixture]
+    public class OperacoesMatematicasBO_Test
+    {
+        [TestCase(1, 1, 1)]
+        [TestCase(10, 2, 20)]
+        [TestCase(10, 10, 1000)]
+        public void Multiplicacao(int entrada1 ,int entrada2, int resultadoEsperado)
+        {
+            OperacoesMatematicasBO negocio = new OperacoesMatematicasBO();
+            var resultadoMetodo = negocio.Multiplicacao(entrada1, entrada2);
+            Assert.AreEqual(resultadoEsperado, resultadoMetodo);
+        }
+    }
+}
---
+++ ExemploTDD/Negocios/OperacoesMatematicasBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExemploTDD.Negocios
+{
+    public class OperacoesMatematicasBO
+    {
+        public int Multiplicacao(int valor1,int valor2)
+        {
+            return valor1* valor2;            
+        }
+    }
+}
